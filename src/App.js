044b116eafecae1dba8e879f9864c932e89282c7
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2019-12-23 18:49:10 -0400

    Created react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HelloWorld from './containers/helloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+export default App;
